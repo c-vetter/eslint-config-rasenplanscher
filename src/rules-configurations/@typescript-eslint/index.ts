40eb7c4975c15619ac5e191a9210e47105b808c8
Christian Vetter <christian@rasenplanscher.info>
2021-06-13 21:56:28 +0200

    feat(rule): @typescript-eslint/consistent-type-definitions

--- src/rules-configurations/@typescript-eslint/index.ts
+++ src/rules-configurations/@typescript-eslint/index.ts
@@ -1,7 +1,9 @@
 import _braceStyle_ from './brace-style'
+import _consistentTypeDefinitions_ from './consistent-type-definitions'
 import _indent_ from './indent'
 
 export default [
 	_braceStyle_,
+	_consistentTypeDefinitions_,
 	_indent_
 ]
