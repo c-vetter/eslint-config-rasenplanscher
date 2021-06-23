2e1e419ebafb6446e78741539f70f7d1d369ac9f
Christian Vetter <christian@rasenplanscher.info>
2021-06-23 20:50:25 +0200

    feat(rule): no-duplicate-case

---
+++ src/rules-configurations/eslint/no-duplicate-case.d.ts
@@ -0,0 +1,7 @@
+import { RuleConfiguration } from '../../../support/Rule'
+
+type Options = never[]
+
+type Configuration = RuleConfiguration<'no-duplicate-case', 'eslint', Options>
+
+export default Configuration
